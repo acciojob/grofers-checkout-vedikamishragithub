8f962474edbf1b7a5af9f35ffdf91414137440f3
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2025-09-16 01:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+   const price=document.querySelectorAll('.price');
+	let total=0;
+	price.forEach(el=>{
+		const val=parseFloat(el.textContent);
+		if(!isNaN(val)){
+         total+=val;
+		}
+	});
+  const newRow=document.createElement('tr');
+  newRow.classList.add('total-row');
+	const labelCell=document.createElement('td');
+	labelCell.textContent="Total";
+	const valueCell=document.createElement('td');
+	valueCell.textContent=total;
+	newRow.appendChild(labelCell);
+	newRow.appendChild(valueCell);
+	table.appendChild(newRow);
+	
+	
+	
+   	 
 };
 
 getSumBtn.addEventListener("click", getSum);
